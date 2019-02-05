8851fa84a535efbef7e5b790d40a10f9b627d93c
Gingerguy <GingerguyAndCo@gmail.com>
2019-02-05 20:11:41 +0300

    Create doyoutrustme.js

---
+++ commands/doyoutrustme.js
@@ -0,0 +1,11 @@
+module.exports = {
+   if (message.author.username == "Memeoverlord"){
+       message.channel.send("of course i dont trust you you fucking cuck");
+       message.channel.send("who the fuck trusts Meme anyways");
+       return;
+     }
+     if (message.isAdmin) {
+      message.channel.send("I trust you");
+    } else {message.channel.send("I do not trust you");};
+  }
+}
