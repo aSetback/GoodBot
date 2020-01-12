085ca2e13e593f93cd5245c482e40a57bf68f337
Dave DeHaan <david@redvanworkshop.com>
2020-01-12 03:06:31 -0500

    Another attempt at credentials fix

--- commands/exportsheet.js
+++ commands/exportsheet.js
@@ -96,7 +96,7 @@ exports.run = (client, message, args) => {
 
 		async.series([
 			function setAuth(step) {
-				var creds = ("~/google.json");
+				var creds = ("./google.json");
 				doc.useServiceAccountAuth(creds, step);
 				
 			},
