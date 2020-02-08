eb5830f67365902bbed9511129898338277c3011
Dave DeHaan <david@redvanworkshop.com>
2020-02-08 02:30:13 -0500

    mis-called model

--- commands/epgp/import.js
+++ commands/epgp/import.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
 
 	const epgpPath = client.config.epgpBackupFolder;
 	message.delete();
-    client.epgp.sync({force: true});
+    client.models.epgp.sync({force: true});
     fs.readdir(epgpPath, (err, files) => {
 		files.sort();
 		files.forEach(file => {
