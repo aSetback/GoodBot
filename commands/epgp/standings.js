8bf1a6271adb676c463745523c8f0d5aad64b26e
Dave DeHaan <david@redvanworkshop.com>
2020-02-11 18:02:16 -0500

    Fixed EPGP standings

--- commands/epgp/standings.js
+++ commands/epgp/standings.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { Op } = require('sequelize');
 
 exports.run = (client, message, args) => {
 	message.delete();
@@ -12,26 +13,39 @@ exports.run = (client, message, args) => {
 
 	let oneMonthAgo = new Date();
 	oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-	oneMonthAgo = oneMonthAgo.toISOString().slice(0, 19).replace('T', ' ');
+	// oneMonthAgo = oneMonthAgo.toISOString().slice(0, 19).replace('T', ' ');
 
-	client.models.epgp.findAll({'where': {'class': args[0], 'guildID': parseInt(message.guild.id)}, 'group': ['guildID', 'player'], 'order': [['createdAt', 'DESC']]}).then((classStandings) => {
-		let returnMessage = "";
-		classStandings.sort((a, b) => {
-			if (a.pr > b.pr) { return -1; }
-			if (b.pr > a.pr) { return 1; }
-			return 0;
+	client.models.epgp.findAll({'where': {'class': args[0], 'guildID': parseInt(message.guild.id), 'createdAt': {[Op.gte]: oneMonthAgo}}}).then((classStandings) => {
+		let standings = [];
+		classStandings.forEach((standing) => {
+			if (standings.indexOf(standing.player) < 0) {
+				standings[standing.player] = standing;
+			} else {
+				let oldStanding = standings[standing.player] 
+				if (oldStanding.createdAt > standing.createdAt) {
+					standings[standing.player] = standing;
+				}
+			}
 		});
-		classStandings.forEach((classStanding) => {
+
+		let arrayStandings = [];
+		for (key in standings) {
+			arrayStandings.push(standings[key]);
+		}
+		
+		let returnMessage = "";
+		arrayStandings.forEach((classStanding) => {
 			if (!returnMessage.length) {
 				returnMessage += "```\n"
-				returnMessage += "Name".padEnd(20) + "Class/Spec".padEnd(25) + "EP".padEnd(15) + "GP".padEnd(15) + "PR".padEnd(15) + "\n";
+				returnMessage += "Name".padEnd(20) + "Class/Spec".padEnd(25) + "EP".padEnd(15) + "GP".padEnd(15) + "PR".padEnd(15) + "Last Seen" + "\n";
 			}
 			let spec = client.config.validSpecs.indexOf(classStanding.spec) >= 0 ? classStanding.spec + " " : "";
 			let pr = classStanding.pr.toString().padEnd(15);
 			pr = Math.round(pr * 100) / 100;
 			pr = pr.toString();
+			let lastSeen = classStanding.createdAt.toISOString().slice(0, 19).replace('T', ' ');
 
-			returnMessage += classStanding.player.padEnd(20) + (spec + classStanding.class).padEnd(25) + classStanding.ep.toString().padEnd(15) + classStanding.gp.toString().padEnd(15) + pr + "\n";
+			returnMessage += classStanding.player.padEnd(20) + (spec + classStanding.class).padEnd(25) + classStanding.ep.toString().padEnd(15) + classStanding.gp.toString().padEnd(15) + pr.padEnd(15) + lastSeen + "\n";
 			if (returnMessage.length > 1500) {
 				returnMessage += '```';
 				message.author.send(returnMessage);
