1a65d62af2e57261f918722da96dfe628de80ac3
Dave DeHaan <david@redvanworkshop.com>
2020-02-11 13:07:01 -0500

    Import only one file every 5s

--- commands/epgp/import.js
+++ commands/epgp/import.js
@@ -7,11 +7,15 @@ exports.run = (client, message, args) => {
     client.models.epgp.sync({force: true});
     fs.readdir(epgpPath, (err, files) => {
 		files.sort();
+		timeout = 0;
 		files.forEach(file => {
+			timeout++;
+			setTimeout(() => {
             if (file.indexOf('.json') >= 0) {
 				console.log('Importing ' + file);
                 client.epgp.parseFile(client, epgpPath + '/' + file);
 			}
+			}, 5000 * timeout);
 		});
 	});
 }
