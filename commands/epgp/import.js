5349708feb8fbbb96d6d5ca9e8dfc4c6eeda5f22
Dave DeHaan <david@redvanworkshop.com>
2020-02-11 15:27:32 -0500

    Disabled import command.

--- commands/epgp/import.js
+++ commands/epgp/import.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
 exports.run = (client, message, args) => {
-
+	return false;
 	const epgpPath = client.config.epgpBackupFolder;
 	message.delete();
     client.models.epgp.sync({force: true});
     fs.readdir(epgpPath, (err, files) => {
 		files.sort();
 		timeout = 0;
-		files.forEach(file => {
+		Afiles.forEach(file => {
             if (file.indexOf('.json') >= 0) {
 				timeout++;
 				setTimeout(() => {
