7acb7e9e052eeb73a58d60161b7c399169f25ec2
Dave DeHaan <david@redvanworkshop.com>
2020-03-10 11:41:01 -0400

    Fixed variable collision

--- commands/signups/remove.js
+++ commands/signups/remove.js
@@ -9,10 +9,10 @@ exports.run = (client, message, args) => {
 
 	message.delete().catch(O_o=>{}); 
 	
-	const raid = message.channel.name;
+	const channel = message.channel.name;
 	const user = args[0] ? args[0] : message.member.displayName;
 	const userName = user.charAt(0).toUpperCase() + user.slice(1).toLowerCase();
-	const fileName = './signups/' + message.guild.id + '-' + raid + '.json';
+	const fileName = './signups/' + message.guild.id + '-' + channel + '.json';
 	client.models.raid.findOne({ where: { 'channelID': message.channel.id, 'guildID': message.channel.guild.id } }).then((raid) => {
 		if (raid) {
 			let parsedLineup = {};
@@ -23,7 +23,7 @@ exports.run = (client, message, args) => {
 			
 			delete parsedLineup[userName];
 			fs.writeFileSync(fileName, JSON.stringify(parsedLineup));
-			client.embed.update(message, raid);
+			client.embed.update(message, channel);
 		} else {
 			message.channel.send("This command can only be used in a sign-up channel.");
 			return false;
