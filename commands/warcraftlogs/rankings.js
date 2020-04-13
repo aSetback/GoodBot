a1e42b7afa7caec830b105d33188095eadb07945
Dave DeHaan <david@redvanworkshop.com>
2020-04-13 18:52:12 -0400

    Fixed an issue with rankings

--- commands/warcraftlogs/rankings.js
+++ commands/warcraftlogs/rankings.js
@@ -101,7 +101,7 @@ exports.run = (client, message, args) => {
         let rankingCount = 0;
         logs.forEach(function (log) {
           if ((isTank && log.spec.toLowerCase() == 'tank')
-            || (!isTank && log.spec.toLowerCase() == 'dps' && metric.toLowerCase() != 'hps')
+            || (!isTank && metric.toLowerCase() != 'hps' && log.spec.toLowerCase() != 'tank')
             || (metric.toLowerCase() == 'hps' && log.spec.toLowerCase() == 'healer')) {
             returnLines++;
             let ranking = log.rank + ' of ' + log.outOf;
