28dd0d3081de58f0b1661d36ce52da0208acb6c1
Dave DeHaan <david@redvanworkshop.com>
2020-04-21 18:30:33 -0400

    Added nexushub support to reserve functionality

--- commands/reserves/reserve.js
+++ commands/reserves/reserve.js
@@ -10,7 +10,7 @@ exports.run = async function(client, message, args) {
     // Find this player in the sign-up
     client.models.signup.findOne({where: {raidID: raid.id, player: player}}).then((signup) => {
         if (signup) {
-            client.models.reserveItem.findOne({where: {raid: raid.raid, name: item}}).then((reserveItem) => {
+            client.models.reserveItem.findOne({where: {raid: raid.raid, name: item}}).then(async (reserveItem) => {
                 if (reserveItem) {
                     record = {
                         raidID: raid.id,
@@ -22,24 +22,45 @@ exports.run = async function(client, message, args) {
                     client.models.raidReserve.findOne({where: {signupID: signup.id, raidID: raid.id}}).then((raidReserve) => {
                         if (raidReserve) { 
                             client.models.raidReserve.update({reserveItemID: reserveItem.id}, {where: {id: raidReserve.id}});
-                            return message.author.send("Your reserve has been updated to " + item + ".");
+                            return message.author.send('**' + client.general.ucfirst(player) + '**\'s reserve has been updated to  **' + item + '** for raid **' + raid.raid + '**.');
                         } else {
                             client.models.raidReserve.create(record).then((record) => {
-                                return message.author.send("Your reserve for " + item + " has been added.");
+                                return message.author.send('A reserve for **' + item + '** has been added for **' + client.general.ucfirst(player) + '** for raid **' + raid.raid + '**.');
                             });       
                         }
                     });
 
                 } else {
-                    return message.channel.send("Unable to find " + item + " in the list of available items for " + raid.raid.toUpperCase());
+                    // Attempt to find it on Nexushub
+                    let itemInfo = await client.nexushub.item(item);
+                    client.models.reserveItem.findOne({where: {raid: raid.raid, name: itemInfo.name}}).then((reserveItem) => {
+                        if (reserveItem) {
+                            record = {
+                                raidID: raid.id,
+                                reserveItemID: reserveItem.id,
+                                signupID: signup.id 
+                            };
+        
+                            // Check if the player already has an existing reserve
+                            client.models.raidReserve.findOne({where: {signupID: signup.id, raidID: raid.id}}).then((raidReserve) => {
+                                if (raidReserve) { 
+                                    client.models.raidReserve.update({reserveItemID: reserveItem.id}, {where: {id: raidReserve.id}});
+                                    return message.author.send('**' + client.general.ucfirst(player) + '**\'s reserve has been updated to **' + itemInfo.name + '** for raid **' + raid.raid + '**.');
+                                } else {
+                                    client.models.raidReserve.create(record).then((record) => {
+                                        return message.author.send('A reserve for **' + itemInfo.name + '** has been added for **' + client.general.ucfirst(player) + '** for raid **' + raid.raid + '**.');
+                                    });       
+                                }
+                            });
+                        } else {
+                            return message.channel.send("I'm sorry, I was unable to find " + item + " in the list of available items for " + raid.raid.toUpperCase());
+                        }
+
+                    });
                 }
             });
-
-        
-        } else {
+      } else {
             return message.channel.send("We couldn't find " + player + " in the sign-ups for this raid.");
         }
     })
-
-
 };
