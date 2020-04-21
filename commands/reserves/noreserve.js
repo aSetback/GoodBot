d05ee6707e0ec34236afe479a492545d1a03d842
Dave DeHaan <david@redvanworkshop.com>
2020-04-21 18:52:08 -0400

    Added a '+noreserve' command

---
+++ commands/reserves/noreserve.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+exports.run = async function(client, message, args) {
+	if (!client.permission.manageChannel(message.member, message.channel)) {
+		return false;
+	}
+
+	let raid = await client.signups.getRaid(client, message.channel);
+    let includes = [
+        {model: client.models.signup, as: 'signup', foreignKey: 'signupID'},
+    ];
+    
+    client.models.raidReserve.findAll({where: {RaidID: raid.id}, include: includes}).then((raidReserves) => {
+        let hasReserve = [];
+        for (key in raidReserves) {
+            hasReserve.push(raidReserves[key].signup.player);
+        }
+        
+        client.models.signup.findAll({where: {RaidID: raid.id, signup: 'yes'}}).then(async (signups) => {
+            let noReserve = [];
+            for (key in signups) {
+                let signup = signups[key];
+                if (!hasReserve.includes(signup.player)) {
+                    noReserve.push(signup.player);
+                }
+            }
+            if (noReserve.length) {
+                let notifyList = await client.notify.makeList(client, message.guild, noReserve);
+                message.channel.send('The following players still need to reserve an item:');
+                message.channel.send(notifyList);
+            } else {
+                message.channel.send('All signed up players have a reserve.');
+            }
+        });
+    });
+
+};
