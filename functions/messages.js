f36d81fc1037253bf6e8c419e22ebf640611156f
Dave DeHaan <david@redvanworkshop.com>
2020-07-07 19:50:39 -0400

    Added a .catch() to the message functions

--- functions/messages.js
+++ functions/messages.js
@@ -2,7 +2,7 @@ module.exports = {
     errorMessage: (channel, message, seconds) => {
         channel.send(message).then((sentMessage) => {
             setTimeout(() => {
-                sentMessage.delete();
+                sentMessage.delete().catch(O_o => { });
             }, seconds * 1000);
         });
         let errorChannel = channel.guild ? channel.guild.channels.find(c => c.name == "error-logs") : null;
@@ -13,7 +13,7 @@ module.exports = {
     send: (channel, message, seconds) => {
         channel.send(message).then((sentMessage) => {
             setTimeout(() => {
-                sentMessage.delete();
+                sentMessage.delete().catch(O_o => { });
             }, seconds * 1000);
         });
     },
