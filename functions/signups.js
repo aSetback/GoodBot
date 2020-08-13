c109a653311acac25bcbe3fdd05370be13d9b3a4
Dave DeHaan <david@redvanworkshop.com>
2020-08-13 18:21:18 -0400

    Added the ability to sign up with your character on either server

--- functions/signups.js
+++ functions/signups.js
@@ -34,9 +34,15 @@ z           }
         // Check to make sure class & role is set
         let character = await client.set.getCharacter(client, message.guild, characterName);
         if (!character && raid.crosspostID && raid.crosspostID.length) {
-            let crosspostChannel = await client.channels.find(c => c.id == raid.crosspostID);
-            character = await client.set.getCharacter(client, crosspostChannel.guild, characterName);
+            let otherChannel;
+            if (message.channel.id == raid.channelID) {
+                otherChannel = await client.channels.find(c => c.id == raid.crosspostID);
+            } else {
+                otherChannel = await client.channels.find(c => c.id == raid.channelID);
+            }
+            character = await client.set.getCharacter(client, otherChannel.guild, characterName);
         }
+
         // Check if the player is avlid
         if (!client.set.validName(characterName)) {
             let playerMessage = 'Unable to sign "' + characterName + '" for this raid.  Please set your in-game name using +nick first.';
