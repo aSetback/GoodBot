aeb9102290171a0c61f10d9cba60472ca13cf2c3
Dave DeHaan <david@redvanworkshop.com>
2020-09-01 17:19:56 -0400

    Added DND EPGP Parse

--- functions/epgp.js
+++ functions/epgp.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const parser = require('luaparse');
 const Discord = require("discord.js");
 
 module.exports = {
@@ -86,5 +86,100 @@ module.exports = {
 				returnMsg += '```';
 				channel.send(returnMsg);
 			});
+	},
+	dndParse: (client, fileData) => {
+		// remove comments
+		fileData = fileData.replace(/(-- \[)(.*)(\])/g, '');
+
+		// Parse lua to json
+		let result = parser.parse(fileData);
+		
+		// Retrieve the relevant data
+		let data = result.body[0].init[0];
+		data = client.epgp.getFieldValue(data.fields, '"namespaces"');
+		data = client.epgp.getFieldValue(data, '"log"');
+		data = client.epgp.getFieldValue(data, '"profiles"');
+		data = client.epgp.getFieldValue(data, '"Drunk N Disorderly"');
+		data = client.epgp.getFieldValue(data, '"snapshot"');
+		data = client.epgp.getFieldValue(data, '"roster_info"');
+		
+		// Loop through results to put it in a friendly json format for display
+		let epgp = [];
+		for (key in data) {
+			let entry = {
+				'player': data[key].value.fields[0].value.raw.replace(/\"/g, ''),
+				'class': data[key].value.fields[1].value.raw.replace(/\"/g, ''),
+				'ep': parseInt(data[key].value.fields[2].value.raw.replace(/\"/g, '').split(',')[0]),
+				'gp': parseInt(data[key].value.fields[2].value.raw.replace(/\"/g, '').split(',')[1])
+			}
+			entry.ep = isNaN(entry.ep) ? 0 : entry.ep;
+			entry.gp = isNaN(entry.gp) ? 0 : entry.gp;
+			if (entry.gp < 200) { entry.gp = 200; }
+			entry.class = client.general.ucfirst(entry.class);
+			if (entry.class == 'Deathknight') { entry.class = 'Death Knight'; }
+			entry.pr = entry.gp == 0 ? 0 : (entry.ep / entry.gp).toPrecision(4);
+			if (entry.pr != 0) {
+				epgp.push(entry);
+			}
+		}
+		epgp.sort((a, b) => {
+			return parseFloat(a.pr) > parseFloat(b.pr) ? -1 : 1;
+		});
+		return epgp;
+	},
+	getFieldValue: (obj, type) => {
+		for (key in obj) {
+			if (obj[key].key.raw == type) {
+				let returnData = obj[key];
+				if (returnData.hasOwnProperty('value')) {
+					returnData = returnData.value;
+				}
+				if (returnData.hasOwnProperty('fields')) {
+					returnData = returnData.fields;
+				}
+				return returnData;
+			}        
+		}
+	},
+	clearChannel: async (displayChannel) => {
+		let messages = await displayChannel.fetchMessages();
+		try {
+			displayChannel.bulkDelete(messages);
+		} catch(error) {
+			messages.array().forEach((message) => {
+				messages.delete();
+			});
+		}
+	},
+	display: (displayChannel, jsonData) => {
+		if (!displayChannel) { return; }
+		let returnMsg = '';
+
+		for (key in jsonData) {
+			if (returnMsg.length > 1900) {
+				returnMsg += '```';
+				displayChannel.send(returnMsg);
+				returnMsg = '';
+			}
+
+			if (!returnMsg) {
+				returnMsg = '```md\n';
+				returnMsg += 'Player'.padEnd(18);
+				returnMsg += 'Class'.padEnd(18);
+				returnMsg += 'EP'.padEnd(10);
+				returnMsg += 'GP'.padEnd(10);
+				returnMsg += 'PR'.padEnd(10) + '\n';	
+				returnMsg += ''.padEnd(68, '-') + '\n';
+			}
+
+			let standing = jsonData[key];
+			returnMsg += standing.player.padEnd(18);
+			returnMsg += standing.class.padEnd(18);
+			returnMsg += standing.ep.toString().padEnd(10);
+			returnMsg += standing.gp.toString().padEnd(10);
+			returnMsg += standing.pr.toString().padEnd(10) + '\n';
+		}
+		returnMsg += '```';
+		displayChannel.send(returnMsg);
 	}
 }
--- functions/watch.js
+++ functions/watch.js
@@ -9,22 +9,27 @@ module.exports = {
 			return false;
 		}
 		let uploadPath = client.config.epgpBackupFolder + '/upload/';
-		fs.watch(uploadPath, {interval: 5000}, (eventType, fileName) => {
+		fs.watch(uploadPath, {interval: 5000}, async (eventType, fileName) => {
 			if (!fsTimeout) {
 				let guildID = fileName.split('-').shift();
-				let guild = client.guilds.get(guildID);
+				let guild = await client.guilds.get(guildID);
 				let channel = {
 					guild: guild,
 					type: null
 				};
-				fsTimeout = setTimeout(() => { 
+				let displayChannel = await guild.channels.find(channel => channel.name === "standings");
+
+				fsTimeout = setTimeout(async () => { 
 					fsTimeout=null
 					let content = fs.readFileSync(uploadPath + fileName, 'utf8');
-					if (fileName.indexOf('GoodEPGP') > -1) {
+					if (content.indexOf('EPGP_DB =') > -1) {
+						let jsonData = client.epgp.dndParse(client, content);
+						await client.epgp.clearChannel(displayChannel);
+						client.epgp.display(displayChannel, jsonData);
+					} else if (fileName.indexOf('GoodEPGP') > -1) {
 						console.log('Parse EPGP: ' + fileName);
 						client.epgp.update(client, guild, content);
-					}
-					if (fileName.indexOf('GoodBot') > -1) {
+					} else if (fileName.indexOf('GoodBot') > -1) {
 						client.log.write(client, null, channel, "Guild bank information has been uploaded.");
 						client.guildbank.update(client, guild, content);
 					}
