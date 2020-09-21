24e4e2d6f7cb3b73803e2a9a7e7fdff7dff4e200
Dave DeHaan <david@redvanworkshop.com>
2020-09-21 17:41:25 -0400

    Implemented new API for gear

--- commands/warcraftlogs/gear.js
+++ commands/warcraftlogs/gear.js
@@ -3,35 +3,29 @@ var moment = require('moment');
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-  let string = args.join(' ');
-  let hidden = false;
-  if (string.indexOf('-h') > -1) {
-    hidden = true;
-    string = string.replace('-h', '');
-    args = string.split(' ');
-  }
 
   let server = await client.customOptions.get(client, message.guild, 'server');
-  let region = await client.customOptions.get(client, message.guild, 'region');
   if (!server) {
     server = 'Mankrik';
   }
-  if (!region) {
-    region = 'US';
-  }
-
-  let player = args[0];
   if (args[1]) {
     server = args[1];
+  }
+
+  let region = await client.customOptions.get(client, message.guild, 'region');
+  if (!region) {
+    region = 'US';
   }
   if (args[2]) {
     region = args[2];
   }
 
-  gearCheck(player, server, region, 0);
+  let player = client.general.ucfirst(args[0]);
 
-  function gearCheck(player, server, region, logKey) {
-    let searchUrl = "https://classic.warcraftlogs.com:443/v1/parses/character/" + player + "/" + server + "/" + region + "?zone=1002&includeCombatantInfo=1&api_key=" + client.config.warcraftlogs;
+  getGearData(player, server, region);
+
+  function getGearData(player, server, region) {
+    let searchUrl = "https://goodbot.me/api/gear/" + player + "/" + server + "/" + region + "?id=" + client.config.goodbot.id + "&key=" + client.config.goodbot.key;
     if (!player) {
       return message.channel.send('Please add a valid player name, eg "+gear Taunt"');
     }
@@ -43,110 +37,45 @@ exports.run = async (client, message, args) => {
       if (err) {
         return;
       }
-      logs = JSON.parse(resp.body);
-      if (logs.error) {
-        return message.channel.send(logs.error);
+      let apiData = JSON.parse(resp.body);
+
+      if (apiData.error) {
+        return message.channel.send(apiData.error);
       }
+      displayData(apiData);
 
-      if (!logs || !logs.length) {
-        return message.channel.send('Could not find gear information for ' + player);
-      }
-
-      logs.sort(function (a, b) {
-        if (a.startTime < b.startTime) { return 1; }
-        else if (a.startTime > b.startTime) { return -1; }
-        else { return 0; }
-      });
-
-      reports = {};
-      logs.forEach((log) => {
-        if (!reports[log.reportID]) {
-          reports[log.reportID] = [];
-        }
-        reports[log.reportID].push(log);
-      });
-
-      let gearReturned = false;
-      for (key in reports) {
-        let report = reports[key];
-        if (!gearReturned) {
-          gearReturned = parseReport(report);
-        }
-      }
-
-      if (!gearReturned) {
-        return message.channel.send('Could not find gear information for ' + player);
-      }
     });
   }
 
-  function parseReport(report) {
+  function displayData(apiData) {
     let embed = new Discord.RichEmbed()
       .setTitle(player)
       .setColor(0x02a64f);
 
     let gearList = {};
-    let slots = [
-      'Head', // 0
-      'Neck', // 1
-      'Shoulders', //2
-      'Shirt', // 3
-      'Chest', // 4
-      'Belt', // 5
-      'Legs', // 6
-      'Boots', // 7
-      'Bracers', // 8
-      'Gloves', // 9
-      'Rings', // 10
-      'Rings', // 11
-      'Trinkets', // 12
-      'Trinkets', // 13
-      'Cloak', // 14
-      'Main Hand', // 15
-      'Off Hand', // 16
-      'Ranged', // 17
-    ];
-    report.forEach(function (log) {
-      log.gear.forEach(function (gear, key) {
-        if (gear.id) {
-          let gearLink = '[' + gear.name + '](' + 'https://classic.wowhead.com/item=' + gear.id + ')';
-          let slotName = slots[key];
-
-          // Create a blank array if this slot isn't already populated.
-          if (!gearList[slotName]) {
-            gearList[slotName] = [];
-          }
-
-          // Add this gear link to the slot array if it's not already there
-          if (gearList[slotName].indexOf(gearLink) < 0) {
-            gearList[slotName].push(gearLink);
-          }
-        }
-      });
+    // Organize our gear by slot
+    apiData.data.forEach(function(fight) {
+        fight.gear.forEach(function(gear) {
+            if (gear.itemName) {
+                if (!gearList[gear.slot]) {
+                    gearList[gear.slot] = [];
+                }
+                gear.link = '[' + gear.itemName + '](' + 'https://classic.wowhead.com/item=' + gear.id + ')';
+                if (gearList[gear.slot].indexOf(gear.link) < 0) {
+                    gearList[gear.slot].push(gear.link);
+                }
+            }
+        });
     });
-    
-    // Filter out empty reports
-    if (!Object.keys(gearList).length) {
-      return false;
-    }
-
-    info = '```Class: ' + report[0].class + '\n';
-    info += 'iLvl:  ' + report[0].ilvlKeyOrPatch + '\n```';
-    embed.addField('**General**', info, false);
 
     for (slot in gearList) {
-      gear = gearList[slot];
-      embed.addField('**' + slot + '**', gear.join('\n'), true);
+      let gear = gearList[slot];
+      if (gear.join().trim().length) {
+        embed.addField('**' + slot + '**', gear.join('\n'), true);
+      }
     }
 
-    let logDate = new Date(report[0].startTime);
-    embed.setFooter('Last seen: ' + moment(logDate).format('LL'));
-    if (hidden) {
-      message.author.send(embed);
-      return true;
-    } else {
-      message.channel.send(embed);
-      return true;
+    embed.setFooter('Last seen: ' + apiData.raidTime.split(' ')[0]);
+      return message.channel.send(embed);
     }
   }
-};
