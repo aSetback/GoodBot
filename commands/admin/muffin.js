e95ec2b4c2b2af5661675e03422bc9d48ca103f3
Dave DeHaan <david@redvanworkshop.com>
2021-06-19 15:45:08 -0400

    Added ascii

--- commands/admin/muffin.js
+++
@@ -1,7 +0,0 @@
-exports.run = async (client, message, args) => {
-    let muffin = new client.disbut.MessageButton()
-        .setStyle('blurple')
-        .setLabel('Muffin Button') 
-        .setID('muffin');
-    message.channel.send('Do Not Push', muffin);
-}
