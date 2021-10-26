f8a383ebe21cb78fa2fc73d0b76738f005475b2f
Dave DeHaan <david@redvanworkshop.com>
2021-10-26 17:04:18 -0400

    Cleaned up the +play command

--- commands/music/play.js
+++ commands/music/play.js
@@ -6,38 +6,55 @@ const { AudioPlayerStatus } = require('@discordjs/voice');
 const ytdl = require('ytdl-core-discord');
 
 exports.run = async (client, message, args) => {
-
+    // Make sure this isn't a DM
 	if (!message.member) {
 		return false;
 	}
 
+    // Verify the user is in a voice chat
 	var vc = message.member.voice.channel;
-	if (!vc) {
+    if (!vc) {
 		return client.messages.send(message.channel, 'You must be in a voice channel to play a youtube URL.', 240);
 	}
 	
+    // Verify the song URL is a youtube link
 	if (!args[0]) {
 		return client.messages.send(message.channel, 'You need to include which youtube URL you want played.', 240);
 	}
 	
+    // Verify it's a valid URL
 	let songURL = args[0];
+    if (songURL.indexOf('https://youtube.com') != 0) {
+        return client.messages.send(message.channel, 'You need to include which youtube URL you want played.', 240);
+    }
+
+    // Join the voice channel
     let connection = joinVoiceChannel({
         channelId: vc.id,
         guildId: vc.guild.id,
         adapterCreator: vc.guild.voiceAdapterCreator,
     });
+
+    // Create the audio player
     let player = createAudioPlayer();
+
+    // Create the stream from Youtube
     let stream = await ytdl(songURL, {
         quality: 'highestaudio',
         highWaterMark: 1024 * 1024 * 1,
         filter: 'audioonly'
     });
-    stream.on('error', err => { 
-        return 'Error';
-    });
+
+    // Create our audio resource
     let resource = createAudioResource(stream);
+
+    // Play the audio resource
     player.play(resource);
+
+    // Subscribe this voice connection to the player
     connection.subscribe(player);
+
+    // Add event handling on the song finishing.
     player.on(AudioPlayerStatus.Idle, () => {
         connection.destroy();
         player.stop();
