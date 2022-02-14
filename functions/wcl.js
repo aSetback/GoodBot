df4942faafc9d9b121de1051b3c7f2be9de6ed40
Dave DeHaan <david@redvanworkshop.com>
2022-02-13 22:44:38 -0500

    Added attendance command to GoodBot

--- functions/wcl.js
+++ functions/wcl.js
@@ -1,6 +1,25 @@
 var request = require('request');
 
 module.exports = {
+    attendance: (client, reportID) => {
+        let promise = new Promise((resolve, reject) => {
+            let apiUrl = "https://goodbot.me/api/attendance/" + reportID + "?id=" + client.config.goodbot.id + "&key=" + client.config.goodbot.key;
+            console.log(apiUrl);
+            reqOpts = {
+                url: encodeURI(apiUrl)
+            };
+
+            request(reqOpts, function (err, resp, html) {
+                if (err) {
+                    return;
+                }
+            
+                let apiData = JSON.parse(resp.body);
+                resolve(apiData);
+            });
+        });
+        return promise;
+    },
 	karaParse: (client, player, server, region) => {
         let promise = new Promise((resolve, reject) => {
             let apiUrl = "https://goodbot.me/api/karazhan/" + player + "/" + server + "/" + region + "?id=" + client.config.goodbot.id + "&key=" + client.config.goodbot.key;
