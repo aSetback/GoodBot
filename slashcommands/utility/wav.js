a305b8f4ea0e26be556fcb2084218b1196a95407
Dave DeHaan <david@redvanworkshop.com>
2022-09-03 14:43:32 -0400

    Fix wav command

--- slashcommands/utility/wav.js
+++ slashcommands/utility/wav.js
@@ -1,29 +1,45 @@
-const { MessageActionRow, MessageSelectMenu, Modal, TextInputComponent } = require("discord.js");
+const fs = require('fs');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 let commandData = new SlashCommandBuilder()
     .setName('wav')
-    .setDescription('Open a modal to play a wav.');
+    .setDescription('Play a wav in your voice channel.')
+    .addStringOption(option =>
+		option
+            .setName('wav')
+			.setDescription('WAV name')
+			.setRequired(true)
+    );
 
 exports.data = commandData;
+
 exports.run = async (client, interaction) => {
-    let modal = new Modal()
-        .setCustomId('sc-modal-wav')
-        .setTitle('Play a WAV file');
-    let input1 = new TextInputComponent()
-        .setCustomId('wavName')
-        .setLabel('WAV Name')
-        .setRequired(true)
-        .setStyle('SHORT');
 
-    let ActionRow1 = new MessageActionRow().addComponents(input1);
-    modal.addComponents([ActionRow1]);
-
-    await interaction.showModal(modal);
-}
-
-exports.modalResponse = async (client, interaction) => {
-	const cmd = client.commands.get('wav');
-	cmd.run(client, interaction, interaction.fields.getTextInputValue('wavName'));
-	interaction.deferUpdate();
+    let vc = interaction.member.voice.channel;
+	if (!vc) {
+        return interaction.reply({content: 'You must be in a voice channel to play a wav file.', ethereal: true});
+	}
+	
+	let wav = interaction.options.getString('wav').toLowerCase();
+	let filename = './wav/' + wav + '.wav';
+	fs.exists(filename, async (exists) => {
+		if (exists) {
+			let connection = joinVoiceChannel({
+				channelId: vc.id,
+				guildId: vc.guild.id,
+				adapterCreator: vc.guild.voiceAdapterCreator,
+			});
+			let player = createAudioPlayer();
+			let resource = createAudioResource(filename);
+			player.play(resource);
+			connection.subscribe(player);
+			player.on(AudioPlayerStatus.Idle, () => {
+				connection.destroy();
+				player.stop();
+			});
+		} else {
+            interaction.reply({content: 'The request wav file does not exist.', ethereal: true});
+		}
+	})
 }
