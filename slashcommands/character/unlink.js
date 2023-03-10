f993d98bb143a24e5af32633820c3df1ec4f7898
Dave DeHaan <david@redvanworkshop.com>
2023-03-09 20:55:07 -0500

    Add an unlink command

---
+++ slashcommands/character/unlink.js
@@ -0,0 +1,32 @@
+const { MessageActionRow, MessageSelectMenu, Modal, TextInputComponent } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+let commandData = new SlashCommandBuilder()
+    .setName('unlink')
+    .setDescription('Unlink a character\'s alts.')
+    .addStringOption(option =>
+		option
+            .setName('character')
+			.setDescription('input')
+			.setRequired(true)
+    );
+
+exports.data = commandData;
+
+exports.run = async (client, interaction) => {
+	let character = interaction.options.getString('wav').toLowerCase();
+
+    // Retrieve our main
+	let mainCharacter = await client.models.character.findOne({where: {name: character, guildID: interaction.guild.id}})
+    if (!mainCharacter) {
+        return interaction.reply({content: 'Could not find character: ' + character, ephemeral: true});
+    }
+
+    // Set the main character as a "main" if it's not already.
+    if (mainCharacter.mainID) {
+        client.models.character.update({altID: null}, {where: {id: mainCharacter.id}});
+    }
+        
+    return interaction.reply({content: 'Alt unlinked.', ephemeral: true});
+}
+
