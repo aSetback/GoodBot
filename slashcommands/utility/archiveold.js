09ade0a5ea02ff985c7f035e4228fe589982b9f8
Dave DeHaan <david@redvanworkshop.com>
2023-04-01 09:26:31 -0400

    Fixed the success message for archiveold.

--- slashcommands/utility/archiveold.js
+++ slashcommands/utility/archiveold.js
@@ -16,6 +16,7 @@ exports.run = async (client, interaction) => {
     if (category) {
         let newArchives = category.clone();
         category.setName('Archives-Old');
+        interaction.reply({content: 'Archives has been renamed to Archive-Old -- a new Archives category has been created.', ephemeral: true});
     } else {
         let errorArchiveNoChannel = client.loc('errorMaxChannel', "The category **Archives** does not exist, please create the category to use this command.");
         client.messages.errorMessage(interaction.channel, errorArchiveNoChannel, 240);
